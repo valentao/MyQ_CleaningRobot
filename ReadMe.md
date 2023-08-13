c4637baa64dbd97e0405ba34de09deb707c19ba5
Ondrej <valentao1291@gmail.com>
2023-08-13 18:06:06 +0200

    #2 read and write file demo

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,9 @@
 - version 2.5
 
 ## Cleaning Robot
+
+- run cmd
+
+    `cd .\bin\Debug\net7.0.`
+
+    `.\cleaning_robot test1.json test0_result.json`
--- src/cleaning_robot/Program.cs
+++ src/cleaning_robot/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+namespace cleaning_robot;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Number of arguments: {args.Length}");
+
+        if (args.Length == 2)
+        {
+            string inputFileArg = args[0];
+            string outputFileArg = args[1];
+
+            FileInfo inputFile = new FileInfo(inputFileArg);
+            FileInfo outputFile = new FileInfo(outputFileArg);
+            if (inputFile.Exists && inputFile.Extension == ".json")
+            {
+                ReadFile(inputFile);
+            }
+            else
+            {
+                Console.WriteLine($"Argument {inputFileArg} is not valid .json file");
+            }
+
+            if (outputFile.Exists)
+            {
+                Console.WriteLine($"Output file {outputFileArg} already exists");
+            }
+            else
+            {
+                WriteFile(outputFile, inputFile);
+            }
+        }
+        else
+        {
+            Console.WriteLine("the application requires exactly 2 arguments");
+        }
+
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+
+    static void ReadFile(FileInfo file)
+    {
+        File.ReadLines(file.FullName).ToList()
+            .ForEach(line => Console.WriteLine(line));
+    }
+
+    static void WriteFile(FileInfo file, FileInfo inputFile)
+    {
+        List<string> lines = new List<string>();
+
+        File.ReadLines(inputFile.FullName).ToList()
+            .ForEach(line => lines.Add(line));
+
+        string[] linesArray = lines.ToArray();
+
+        File.WriteAllLines(file.FullName, linesArray);
+    }
+}
