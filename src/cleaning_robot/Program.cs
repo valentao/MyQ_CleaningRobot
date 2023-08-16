8499b5a97168b98b731c9438021815bb4bdd9014
Ondrej <valentao1291@gmail.com>
2023-08-16 13:15:22 +0200

    #8 robot refactoring - move methods from Program.cs to Robot class

--- src/cleaning_robot/Program.cs
+++ src/cleaning_robot/Program.cs
@@ -1,8 +1,4 @@
-﻿using Newtonsoft.Json;
-using static cleaning_robot.Movement;
-using static cleaning_robot.Document;
-
-namespace cleaning_robot;
+﻿namespace cleaning_robot;
 class Program
 {
     public static void WriteBatteryStatus(int batteryStatus)
@@ -22,60 +18,97 @@ class Program
             Console.WriteLine($"Argument {i}: {args[i]}");
         }
 
-        Robot robot = Robot.GetRobot();
-        
-        FileInfo jsonFile = new FileInfo("test0.json"); // TODO args[0]
+        if (args.Length == 2)
+        {
+            string inputFileArg = "test0.json";  // TODO: args[0];
+            string outputFileArg = "test0_result.json"; // TODO: args[1];
 
-        Input? input = JsonConvert.DeserializeObject<Input>(Document.Read(jsonFile));
+            FileInfo inputFile = new FileInfo(inputFileArg);
+            FileInfo outputFile = new FileInfo(outputFileArg);
+            if (inputFile.Exists && inputFile.Extension == ".json")
+            {
+                //Robot robot = 
+                Robot.GetRobot();
+                Robot.LoadJson(inputFile);
+                Robot.Start();
+                //ReadFile(inputFile);
+                //string json = Read(inputFile);
+                //Console.WriteLine(json);
+                //var x = JsonConvert.DeserializeObject<Input>(json);
+
+                Robot.SaveJson(outputFile);
+            }
+            else
+            {
+                Console.WriteLine($"Argument {inputFileArg} is not valid .json file");
+            }
+
+            if (outputFile.Exists)
+            {
+                Console.WriteLine($"Output file {outputFileArg} already exists");
+            }
+            //else
+            //{
+            //    WriteFile(outputFile, inputFile);
+            //}
+        }
+        else
+        {
+            Console.WriteLine("the application requires exactly 2 arguments");
+        }
+
+
+        
+        
+//        FileInfo jsonFile = new FileInfo("test0.json"); // TODO args[0]
+
+        //Input? input = JsonConvert.DeserializeObject<Input>(Document.Read(jsonFile));
 
         //robot.Visited = new List<Cell>();
         //robot.Cleaned = new List<Cell>();
 
-        List<List<string>>? mapX = new List<List<string>>(); // TODO
+        //List<List<string>>? mapX = new List<List<string>>(); // TODO
 
-        if (input != null)
-        {
-            mapX = input.map;
-            robot.Battery = input.battery;
-            if (input.start != null)
-            {
-                robot.Position = new Position(input.start.X, input.start.Y, input.start.facing);
-            }
+        //if (input != null)
+        //{
+        //    mapX = input.map;
+        //    robot.Battery = input.battery;
+        //    if (input.start != null)
+        //    {
+        //        robot.Position = new Position(input.start.X, input.start.Y, input.start.facing);
+        //    }
 
-            if (input.commands != null)
-            {
-                Command[]? tmpCmd = new Command[input.commands.Length];
+        //    if (input.commands != null)
+        //    {
+        //        Command[]? tmpCmd = new Command[input.commands.Length];
 
-                for (int i = 0; i < input.commands.Length; i++)
-                {
-                    tmpCmd[i] = Command.GetCommand(input.commands[i]);
-                }
+        //        for (int i = 0; i < input.commands.Length; i++)
+        //        {
+        //            tmpCmd[i] = Command.GetCommand(input.commands[i]);
+        //        }
 
-                robot.CommandsArray = tmpCmd;
-            }
-        }
+        //        robot.CommandsArray = tmpCmd;
+        //    }
+        //}
 
-        if (mapX != null)
-        {
-            Map map = Map.GetMap(mapX);
-        }
-        bool test = Map.IsCellAccessible(robot.Position.X, robot.Position.Y);
-        if (test)
-        {
-            robot.Visited.Add(new Cell(robot.Position.X, robot.Position.Y));
-        }
-
-
-        ;       
+        //if (mapX != null)
+        //{
+        //    Map map = Map.GetMap(mapX);
+        //}
+        //bool test = Map.IsCellAccessible(robot.Position.X, robot.Position.Y);
+        //if (test)
+        //{
+        //    robot.Visited.Add(new Cell(robot.Position.X, robot.Position.Y));
+        //}      
 
         // set start position out of map
         //robot.Position = new Position(3, 1, robot.Position.facing);
 
-        Cell tmpPosition = new Cell(robot.Position.X, robot.Position.Y);
+        //Cell tmpPosition = new Cell(robot.Position.X, robot.Position.Y);
         //string? cellAccessibility = robot?.Map?[tmpPosition.X][tmpPosition.Y];
 
-        tmpPosition = new Cell(0, 5);
-        robot.Position = new Position(tmpPosition.X, tmpPosition.Y, robot.Position.facing);
+        //tmpPosition = new Cell(0, 5);
+        //robot.Position = new Position(tmpPosition.X, tmpPosition.Y, robot.Position.facing);
 
         //int rowCount = robot.Map.Count; // X length
         //int colCount = robot.Map[0].Count(); // Y length
@@ -99,16 +132,12 @@ class Program
         //    }
         //}
         
-
         //var row = robot.Map.Where(r => r[tmpPosition.X])
-
 
         //var element = (from row in robot.Map
         //               from col in row
         //               where col[tmpPosition.X] == tmpPosition.X
         //               select col).FirstOrDefault();
-
-
 
         //var element = (from sublist in userList
         //               from item in sublist
@@ -119,107 +148,10 @@ class Program
 
         //bool v = robot.Map.Exists(robot.Map[0]);
 
-
-        List<List<string>> tmpMap = new List<List<string>>();
+        //List<List<string>> tmpMap = new List<List<string>>();
         //tmpMap.Add();
 
         //robot.Map.Exists(tmpPosition.X, tmpPosition.Y);
-
-        ;
-        
-        
-
-        foreach (Command cmd in robot.CommandsArray)
-        {
-            if (robot.Battery >= cmd.Cost)
-            {
-                robot.Battery -= cmd.Cost;
-
-                if ((cmd == Command.TurnLeft || cmd == Command.TurnRight) && cmd.Turn != 0)
-                {
-                    robot.Position.facing = Turn(robot.Position.facing, cmd.Turn);
-                }
-
-                if (cmd == Command.Advance)
-                {
-                    robot.Position = Advance(robot.Position);
-                }
-
-                if (cmd == Command.Back)
-                {
-                    robot.Position = Back(robot.Position);
-                }
-
-                if (cmd == Command.Clean)
-                {
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Command {cmd.Name} requires more battery ({cmd.Cost}) than is actual capacity {robot.Battery}.");
-                //Document.WriteToJson(robot, new FileInfo(args[1]));
-                break;
-            }
-        }
-
-        if (args.Length == 2)
-        {
-            string inputFileArg = args[0];
-            string outputFileArg = args[1];
-
-            FileInfo inputFile = new FileInfo(inputFileArg);
-            FileInfo outputFile = new FileInfo(outputFileArg);
-            if (inputFile.Exists && inputFile.Extension == ".json")
-            {
-                //ReadFile(inputFile);
-                string json = Read(inputFile);
-                Console.WriteLine(json);
-                var x = JsonConvert.DeserializeObject<Input>(json);
-            }
-            else
-            {
-                Console.WriteLine($"Argument {inputFileArg} is not valid .json file");
-            }
-
-            if (outputFile.Exists)
-            {
-                Console.WriteLine($"Output file {outputFileArg} already exists");
-            }
-            //else
-            //{
-            //    WriteFile(outputFile, inputFile);
-            //}
-        }
-        else
-        {
-            Console.WriteLine("the application requires exactly 2 arguments");
-        }
-
-        Output output = new Output();
-        output.battery = robot.Battery;
-        output.final = robot.Position;
-
-        // TODO - select distinct from 
-        //Cell[] d = robot.Visited.ToArray();
-        //Cell[] distinct = d.SelectMany(a => a).Distinct().ToArray();
-        //var distinct = array.SelectMany(a => a).Distinct().ToArray();
-
-        // TODO - sort Array by X
-        //Cell[] x = robot.Visited.ToArray();
-        //var sorted = x.OrderBy(y => y[0]).ThenBy(y => y[1]).ThenBy(y => y[2]);
-
-        output.visited = robot.Visited.ToArray();
-        output.cleaned = robot.Cleaned.ToArray();
-
-        //Cell c1 = new Cell(1,0);
-        //Cell c2 = new Cell(2, 0);
-        //Cell c3 = new Cell(3, 0);
-        //output.visited = new Cell[] { c1, c2, c3 };
-        //output.cleaned = new Cell[] { c1, c2 };
-
-        string outputJson = JsonConvert.SerializeObject(output);
-        FileInfo fileJsonOutput = new FileInfo("test_output.json"); // TODO new FileInfo(args[1])
-        Document.Write(fileJsonOutput, outputJson);
 
         Console.ReadLine();
         Environment.Exit(0);
--- src/cleaning_robot/Robot.cs
+++ src/cleaning_robot/Robot.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.CommandLine;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using static cleaning_robot.Movement;
 
 namespace cleaning_robot;
 
@@ -24,17 +20,14 @@ public class Robot
     Facing.W
     };
 
-    public enum Commands
-    {
-
-    }
-
-    private static Robot? instance = null;
+    private static Robot? robot = null;
 
     private Robot()
     {
         Visited = new List<Cell>();
         Cleaned = new List<Cell>();
+        HitObstacleCount = 0;
+        IsStucked = false;
     }
 
     public int Battery { get; set; }
@@ -43,9 +36,15 @@ public class Robot
 
     public Command[] CommandsArray { get; set; }
 
+    public Map Map { get; set; }
+
     public List<Cell> Visited { get; set; }
 
     public List<Cell> Cleaned { get; set; }
+
+    public int HitObstacleCount { get; set; }
+
+    public bool IsStucked { get; set; }
 
     //Lock Object
     private static object lockThis = new object();
@@ -53,9 +52,207 @@ public class Robot
     {
         lock (lockThis)
         {
-            if (Robot.instance == null)
-                instance = new Robot();
+            if (Robot.robot == null)
+                robot = new Robot();
         }
-        return instance;
+        return robot;
+    }
+
+    public static void LoadJson(FileInfo file)
+    {
+        Input? input =  JsonConvert.DeserializeObject<Input>(Document.Read(file));
+
+        //List<List<string>>? mapX = new List<List<string>>(); // TODO
+
+        if (input != null && robot != null)
+        {
+            robot.Map = Map.GetMap(input.map);
+            robot.Battery = input.battery;
+            if (input.start != null)
+            {
+                robot.Position = new Position(input.start.X, input.start.Y, input.start.facing);
+            }
+
+            if (input.commands != null)
+            {
+                Command[]? tmpCmd = new Command[input.commands.Length];
+
+                for (int i = 0; i < input.commands.Length; i++)
+                {
+                    tmpCmd[i] = Command.GetCommand(input.commands[i]);
+                }
+
+                robot.CommandsArray = tmpCmd;
+            }
+        }
+
+        bool isCellAccessible = Map.IsCellAccessible(robot.Position.X, robot.Position.Y);
+        if (isCellAccessible)
+        {
+            robot.Visited.Add(new Cell(robot.Position.X, robot.Position.Y));
+        }
+    }
+
+    public static void SaveJson(FileInfo file)
+    {
+        Output output = new Output();
+        output.battery = robot.Battery;
+        output.final = robot.Position;
+
+        // TODO - select distinct from 
+        //Cell[] d = robot.Visited.ToArray();
+        //Cell[] distinct = d.SelectMany(a => a).Distinct().ToArray();
+        //var distinct = array.SelectMany(a => a).Distinct().ToArray();
+
+        // TODO - sort Array by X
+        //Cell[] x = robot.Visited.ToArray();
+        //var sorted = x.OrderBy(y => y[0]).ThenBy(y => y[1]).ThenBy(y => y[2]);
+
+        output.visited = robot.Visited.ToArray();
+        output.cleaned = robot.Cleaned.ToArray();
+
+        //Cell c1 = new Cell(1,0);
+        //Cell c2 = new Cell(2, 0);
+        //Cell c3 = new Cell(3, 0);
+        //output.visited = new Cell[] { c1, c2, c3 };
+        //output.cleaned = new Cell[] { c1, c2 };
+
+        string outputJson = JsonConvert.SerializeObject(output);
+        FileInfo fileJsonOutput = new FileInfo("test_output.json"); // TODO new FileInfo(args[1])
+        Document.Write(fileJsonOutput, outputJson);
+    }
+
+    public static void Start()
+    {
+        foreach (Command cmd in robot.CommandsArray)
+        {
+            //if (robot.Battery >= cmd.Cost)
+            if (IsBatteryEnough(cmd.Cost) && !robot.IsStucked)
+            {
+                Console.WriteLine($"Prepared move {cmd.Name}");
+
+                Move(cmd);      
+                //if (robot.HitObstacleCount >= 5)
+                //{
+                //    robot.IsStucked = 
+                //}
+            }
+            else
+            {
+                if (robot.IsStucked)
+                {
+                    Console.WriteLine("Robot is stucked. End of program.");
+                }
+                else
+                {
+                    Console.WriteLine($"Command {cmd.Name} requires more battery ({cmd.Cost}) than is actual capacity {robot.Battery}.");
+                }
+                    //Document.WriteToJson(robot, new FileInfo(args[1]));
+                break;
+            }
+        }
+    }
+
+    private static bool Move(Command command)
+    {
+        bool isMove = true;
+
+        robot.Battery -= command.Cost;
+
+        if ((command == Command.TurnLeft || command == Command.TurnRight) && command.Turn != 0)
+        {
+            robot.Position.facing = Turn(robot.Position.facing, command.Turn);
+            Console.WriteLine($"{command.Name}");
+        }
+
+        if (command == Command.Advance)
+        {
+            Position position = Advance(robot.Position);
+
+            if (Map.IsCellAccessible(position.X, position.Y))
+            {
+                robot.Position = position;
+                robot.Visited.Add(new Cell(position.X, position.Y));
+                Console.WriteLine($"Advanced to X:{position.X},Y:{position.Y}");
+            }
+            else
+            {
+                isMove = false;
+                BackOff();
+            }
+        }
+
+        if (command == Command.Back)
+        {
+            Position position = Back(robot.Position);
+
+            if (Map.IsCellAccessible(position.X, position.Y))
+            {
+                robot.Position = position;
+                robot.Visited.Add(new Cell(position.X, position.Y));
+                Console.WriteLine($"Returned to X:{position.X},Y:{position.Y}");
+            }
+            else
+            {
+                isMove = false;
+                BackOff();
+            }
+        }
+
+        if (command == Command.Clean)
+        {
+            robot.Cleaned.Add(new Cell(robot.Position.X, robot.Position.Y));
+            Console.WriteLine($"Clean X:{robot.Position.X},Y:{robot.Position.Y}");
+        }
+
+        return isMove;
+    }
+
+    private static void BackOff()
+    {
+        Console.WriteLine($"Back off sequence. Hit obstacle count: {robot.HitObstacleCount}.");
+
+        if (robot.HitObstacleCount >= 5)
+        {
+            robot.IsStucked = true;
+        }
+        else
+        {
+            string[] backOffCommands = BackOffStrategy(robot.HitObstacleCount);
+
+            Command[]? tmpCmd = new Command[backOffCommands.Length];
+
+            bool isMove = true;
+
+            for (int i = 0; i < backOffCommands.Length; i++)
+            {
+                tmpCmd[i] = Command.GetCommand(backOffCommands[i]);
+
+                isMove = Move(tmpCmd[i]);
+
+                if (tmpCmd[i] == Command.Advance || tmpCmd[i] == Command.Back)
+                {
+                    if (isMove)
+                    {
+                        robot.HitObstacleCount = 0;
+                    }
+                    else
+                    {
+                        robot.HitObstacleCount += 1;
+                        break;
+                    }
+                }
+            }
+
+            if (!isMove)
+            {
+                BackOff();
+            }
+        }
+    }
+
+    public static bool IsBatteryEnough(int cost)
+    {
+        return robot?.Battery >= cost;
     }
 }
